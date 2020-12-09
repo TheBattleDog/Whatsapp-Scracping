1a446a2f54b2bae2bf490f085a3c379daa8161e7
unknown <krishnanand7777777@gmail.com>
2020-12-09 08:19:27 +0530

    main.py updated

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 import time
 from datetime import date
 import Functions
@@ -7,8 +10,15 @@ import Functions
 vChrome = webdriver.Chrome(executable_path="C:/Users/AK/Documents/Code/Whatsapp/chromedriver.exe")
 vChrome.get("https://web.whatsapp.com")
 
-time.sleep(10)
+GroupName = Functions.group_nav()
 
-GropName = Functions.group_nav()
+WebDriverWait(vChrome, 20).until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div/div[3]/div/div[1]/div/label/div')))
 
-vChrome.find_element_by_xpath(f'//span[@title="{GropName}"').click()
+SearchBox = vChrome.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div/div[1]/div/label/div')
+SearchBox.click()
+vChrome.find_element_by_xpath('//*[@id="side"]/div[1]/div/label/div/div[2]').send_keys(GroupName)
+
+WebDriverWait(vChrome, 20).until(EC.element_to_be_clickable((By.XPATH, f'//span[@title="{GroupName}"]')))
+GroupElement = vChrome.find_element_by_xpath(f'//span[@title="{GroupName}"]')
+
+GroupElement.click()
