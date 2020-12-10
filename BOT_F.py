4ff40461f917e9364174d08c8d1caa9217daf9a8
TheBattleDog <krishnanand7777777@gmail.com>
2020-12-10 21:39:15 +0530

    BOT_F.py while loop edited and added clear_text_area function inside Class Bot

--- BOT_F.py
+++ BOT_F.py
@@ -10,6 +10,7 @@ class BOT:
         self.vChrome = webdriver.Chrome(executable_path=driver_path)
         self.group_name = "Dumb Dumb"  # dep.f_group_name()
         self.link = ""
+        self.group_search_bar = '//*[@id="app"]/div/div/div[2]/div[3]/span/div/div/div[1]/div/label/div/div[2]'
 
     def url_open(self, link):
         self.vChrome.get(link)
@@ -29,14 +30,20 @@ class BOT:
 
     def nav_to_link(self):
         self.vChrome.find_element_by_xpath('//*[@id="main"]/header/div[3]/div/div[1]/div/span').click()
-        group_search_bar = '//*[@id="app"]/div/div/div[2]/div[3]/span/div/div/div[1]/div/label/div/div[2]'
-        self.vChrome.find_element_by_xpath(group_search_bar).click()
-        self.vChrome.find_element_by_xpath(group_search_bar).send_keys("https://")
-        self.vChrome.find_element_by_xpath(group_search_bar).click()
+        self.vChrome.find_element_by_xpath(self.group_search_bar).click()
+        self.vChrome.find_element_by_xpath(self.group_search_bar).send_keys("https://")
+        self.vChrome.find_element_by_xpath(self.group_search_bar).click()
+
+
+    def clear_text_area(self, xpath_to_clear):
+        area = self.vChrome.find_element_by_xpath(xpath_to_clear)
+        # Clear the Search field by pressing Ctrl + Backspace (Ctrl = u'\ue009' Backspace = u'\ue003')
+        while len(area.text):
+            area.send_keys(u'\ue009' + u'\ue003')
 
 
     def get_link(self):
-        message_time_xpath = '//*[@id="pane-side"]/div[1]/div/div/div[1]/div/div/div/div[1]/div[2]'
+        message_time_xpath = '//div[@class="_2gsiG"]'
         WebDriverWait(self.vChrome, 20).until(EC.element_to_be_clickable((By.XPATH, message_time_xpath)))
         time = self.vChrome.find_element_by_xpath(message_time_xpath).text
 
@@ -47,11 +54,18 @@ class BOT:
         link_xpath = self.vChrome.find_element_by_xpath(
             '//*[@id="pane-side"]/div[1]/div/div/div[1]/div/div/div/div[2]/div[1]/span/span')
 
-        while time != dep.get_current_time() or link_xpath.text[0:24] != "https://meet.google.com/":
+        while True:
             time = self.vChrome.find_element_by_xpath(message_time_xpath).text
             link_xpath = self.vChrome.find_element_by_xpath(
-                '//*[@id="pane-side"]/div[1]/div/div/div[1]/div/div/div/div[2]/div[1]/span/span[3]')
+                '//span[@class="_1hI5g _1XH7x _1VzZY"][@dir="ltr"]')
+            self.clear_text_area(self.group_search_bar)
+            self.vChrome.find_element_by_xpath(self.group_search_bar).send_keys('https://')
+            # add this in instead of 9:00pm dep.get_current_time() | use 24 instead of 7
+            # use https://meet.google.com/ instead of https://
+            if time != dep.get_current_time() and link_xpath.text[0:24] != "https://meet.google.com/":
+                break
 
         self.link = link_xpath.text
         print(self.link)
+
 
