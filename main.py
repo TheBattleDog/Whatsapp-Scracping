52bdab0e5f7306a579785952d1e18365ae01b16e
TheBattleDog <krishnanand7777777@gmail.com>
2020-12-14 22:32:43 +0530

    BOT_F.py updated - added write_link_file() and invoke_js_file()

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from BOT_F import BOT
-
+from BOT_Func import BOT
 
 bot = BOT("C:/Users/AK/Documents/Code/Whatsapp/chromedriver.exe")
 
@@ -7,3 +6,5 @@ bot.url_open("https://web.whatsapp.com")
 bot.nav_to_group()
 bot.nav_to_link()
 bot.get_link()
+bot.write_link_file()
+bot.invoke_js_file()
