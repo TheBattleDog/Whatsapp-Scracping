9c9cb64ffa35c1c2643fa94eb33acac000460bb9
TheBattleDog <67001167+TheBattleDog@users.noreply.github.com>
2021-01-22 23:28:31 +0530

    Changed from chrome to edge support
    The program was based on chrome but now it is based on Edge.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from BOT_Func import BOT
 
-bot = BOT("C:/Users/AK/Documents/Code/Whatsapp/chromedriver.exe")
+bot = BOT("C:/Users/AK PC/Documents/Programmes/Python/WhatsApp_Bot/msedgedriver.exe")
 
 bot.url_open("https://web.whatsapp.com")
 bot.nav_to_group()
